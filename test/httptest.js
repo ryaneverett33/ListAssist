b7eeac1115bd3a7830788b67afacd9f58e4c7d7f
Drew Atkinson <drewatkinson5@gmail.com>
2018-02-11 21:51:36 -0500

    test updates

--- test/httptest.js
+++ test/httptest.js
@@ -20,3 +20,26 @@ describe('homepage', () => {
     });
 
 })
+
+describe('error page', () => {
+    it('shows the error page', function (done) { // <= Pass in done callback
+        chai.request(app)
+            .get('/TESTERRORROUTE')
+            .end(function (err, res) {
+                expect(res).to.have.status(404);
+                done(); // <= Call done to signal callback end
+            });
+    });
+
+})
+
+describe('googlesignin page', () => {
+    it('shows the sign in page', function (done) { // <= Pass in done callback
+        chai.request(app)
+            .get('/googlesignin.html')
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                done(); // <= Call done to signal callback end
+            });
+    });
+})
