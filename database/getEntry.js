93803efba063b5b027201d32b7da682abbdbf6a3
Will-Johnston <johnstw@purdue.edu>
2018-02-18 02:32:05 -0500

    test get entry and bug fixes

--- database/getEntry.js
+++ database/getEntry.js
@@ -41,7 +41,7 @@ Users table columns: id, username
 user_id: unique id of user stored in database, accessible from google login
 callback: function that will be used to return the results of the query
 */
-function getUser(user_id, callback) {
+exports.getUser = function getUser(user_id, callback) {
 	pool.connect(function(error, connection) {
 		//check for errors
 	    if (error) {
@@ -73,6 +73,31 @@ Items table columns: id, name, picture_url, buyer, purchased, list_id
 list_id: unique id of list stored in database, accessible from getting a users list (function below)
 callback: function that will be used to return the results of the query
 */
+exports.getItems = function getItems(list_id, callback) {
+	pool.connect(function(error, connection) {
+		//check for errors
+	    if (error) {
+	      console.error("Error getting user from database: %s", error);
+	      callback(null);
+	      pool.disconnect(connection);
+	      return;
+	    }
+	    else {
+			connection.query('SELECT * FROM Items where list_id=?', [list_id], function(error2, results, fields) {
+		        if (error2) {
+		        	console.log("An error occured getting items for list: %s", error2)
+		        	callback(null);
+		        } else {
+		        	callback(results);
+		        }
+	        	pool.disconnect(connection);
+	        	return;
+		    });
+		}
+	});
+}
+
+//an internal function used for get lists
 function getItems(list_id, callback) {
 	pool.connect(function(error, connection) {
 		//check for errors
@@ -97,34 +122,34 @@ function getItems(list_id, callback) {
 	});
 }
 
+
+
 /*
 This function will return a RowDataPacket, which can be parsed in the following manner: results[<index>].column
 Lists table columns: id, name, user_id
 user_id: unique id of user stored in database, accessible on frontend via google login
 callback: function that will be used to return the results of the query
 */
-function getLists(user_id, callback) {
+exports.getLists = function getLists(user_id, callback) {
 	//for each list, get its items and add them to an array
 	pool.connect(function(error, connection) {
 		//check for errors
 	    if (error) {
-	      console.error("Error getting user from database: %s", error);
-	      callback(false);
+	      console.error("Error getting list from database: %s", error);
+	      callback(null);
 	      return;
 	    }
 	    else {
-	    	var lists = {};  //lists that user has, and it's items within
+	    	var lists = [];  //lists that user has, and it's items within
 	    	var items = [];
 	    	var query = connection.query('SELECT * FROM Lists where user_id=?', [user_id]);
 
 	    	query
 			  .on('error', function(err) {
 			  	console.error("An error occured getting list(s) from database: %s", err);
+			  	callback(null);
+			  	return;
 			    // Handle error, an 'end' event will be emitted after this as well
-			  })
-			  .on('fields', function(fields) {
-			  	console.log("FIELDS: %s", fields)
-			    // the field packets for the rows to follow
 			  })
 			  .on('result', function(row) {
 			    // Pausing the connnection to wait for items query (so that lists does not fill in empty arrays always)
@@ -136,17 +161,16 @@ function getLists(user_id, callback) {
 			    	} else {
 			    		items = [];
 			    	}
-			    	lists[row.name] = items;  //add items to list, if any
+			    	list = {row, items}
+			    	lists.push(list);  //add items to list, if any
 			    	connection.resume();  //resume the connection now that the item query is complete
 			    });
 
 			  })
 			  .on('end', function() {
 			    // all rows have been received
-			    for (var key in lists) {
-			    	console.log(key, lists[key]);
-			    }
-			    console.log("LISTS: %s", lists);
+			    callback(lists);
+			    return;
 			  });
 		}
 	});
